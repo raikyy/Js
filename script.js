6f2e12ccc245be64285ec7dc218bb0dcbadf51be
raikyy <98322401+raikyy@users.noreply.github.com>
2022-10-14 17:09:54 +0500

    homework

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 let i=1;
 while (i<=10) {
     console.log(i);
@@ -15,10 +16,14 @@ for (let i = 0; i <= 20; i++) {
     }
   }
 
+
+
 let name = prompt('Введите ваше имя');
 let town = prompt('Введите ваш город проживания');
 let age = prompt('Введите ваш возраст');
 console.log(`Добрый день ${name}, вы проживаете в городе ${town}, ваш вазраст - ${age}`);
+
+
 
 
 let r = prompt('Сумма товаров в корзине');
@@ -37,6 +42,9 @@ else if (r>2000) {
 else (console.log(`Бесплатная доставка от 1000 рублей, до бесплатной доставки не 
 хватает ${1000-r}`));
 
+
+
+
 let sum = 10000;
 let promo = prompt(`Стоимость товаров в корзине - ${sum} руб. Введите промокод`);
 let p = 0.5;
@@ -47,3 +55,108 @@ if (promo=='DELFA') {
 else {
     console.log('Промокод не подходит');
 }
+
+
+let n = 100
+let num = 0
+while (n>50) {
+    n/=2;
+    num++;
+}
+console.log(num)
+
+
+
+function msg(Username, year) {
+    console.log(`Привет ${Username}, вам ${2022-year} лет`)
+};
+msg('Ярослава', 2006);
+
+
+function ret(a) {
+    if (a>=0) {
+        console.log ('Число положительное')
+    }
+    else (console.log ('Число отрицательное'))
+}
+ret(9)
+*/
+
+
+let i = 1
+if (i=1) {
+    console.log('Переменная i равна 1')
+}
+
+
+
+let r = 4
+if (r<0) {
+    console.log('Значение отрицательное')
+}
+else {
+    console.log('Значение положительное')
+}
+
+
+
+let m = 4
+let n
+if (m>50) {
+    n='большое'
+}
+else {
+    n='маленькое'
+}
+console.log(n)
+
+
+
+let a = 5
+if (a=1) {
+    console.log('Значение равно 1')
+}
+else if (a=2) {
+    console.log('Значение равно 2')
+}
+else if (a=3) {
+    console.log('Значение равно 3')
+}
+else {
+    console.log('Неизвестное значение')
+}
+
+
+
+let s = 45
+while (s>=45, s<=65) {
+    console.log(s)
+    s++
+}
+
+
+
+let w = 45
+while (w>=45, w<=670) {
+   if (w%10==0) { console.log(w) }
+    w++
+}
+
+//Тут что-то страшное, потому что браузер зависает:(
+/*
+for (let i = 45; i++;) {
+    if (i<=65) {
+        console.log (i)
+    }
+    if (i<=670, i%10==0) {
+        console.log (i)
+    }
+}
+*/
+
+
+function hello1(b) {
+    console.log('Привет, JavaScript!')
+}
+
+hello1(a)
